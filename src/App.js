3800aee231f23552a2527c9df6b8618ce8b20fcd
KingSplatt <malp18774@gmail.com>
2024-10-20 17:19:20 -0700

    Prueba

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Log from './Log.js'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Log />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -18,7 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </div >
   );
 }
 
---
+++ src/Log.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Log = () => {
+    return (
+        <div>
+            Esta es unna prueba asdawead
+        </div>
+    )
+}
+
+export default Log
